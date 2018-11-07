2e4e07bd7443d2b25cccf3be16782301016416dc
josetorero <44843990+josetorero@users.noreply.github.com>
2018-11-07 11:44:42 -0500

    Add files via upload

---
+++ 0.Fastest function to check if a number is Prime.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 11:41:23 2018
+
+@author: Jose.Torero
+"""
+
+def isprime(n):
+    """Returns True if n is prime."""
+    if n == 2:
+        return True
+    if n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+    if n % 3 == 0:
+        return False
+    i = 5
+    w = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += w
+        w = 6 - w
+    return True
